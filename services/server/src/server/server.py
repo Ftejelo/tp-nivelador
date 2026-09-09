5b1fb439499da7801746c67a63172082e345feb8
ffer11 <ftejelo@fi.uba.ar>
2026-09-08 22:14:36 -0300

    Ejercicio 7, conexiones y concurrencia basada en threads con estado mutable compartido (lock y condition(condvar))

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import threading
 
 import logger
 import protocol
@@ -14,10 +15,19 @@ class Server:
         self.server_port = server_port
         self.storage_path = os.environ.get("LOTTERY_STORAGE_PATH", "/tmp/lottery_bets.csv")
         self.lottery = Lottery(self.storage_path)
+        self.quorum_min = int(os.environ.get("AGENCY_QUORUM_MIN", "1"))
+        
+        # Thread synchronization
+        self.agencies_finished = 0
+        self.agencies_finished_lock = threading.Lock()
+        self.quorum_condition = threading.Condition(self.agencies_finished_lock)
+        self.winners_calculated = False
+        self.winners = []
 
     def _handle_client(self, client_socket):
         action = "handle-client"
         bets_received = 0
+        client_agency_id = None
         try:
             logger.info(action, logger.LogResult.in_progress)
             while True:
@@ -27,6 +37,8 @@ class Server:
                     # Convert protocol bets to lottery bets
                     lottery_bets = []
                     for bet in payload.bets:
+                        if client_agency_id is None:
+                            client_agency_id = bet.agency_id
                         lottery_bets.append(Bet(
                             agency_id=bet.agency_id,
                             first_name=bet.first_name,
@@ -41,15 +53,50 @@ class Server:
                     bets_received += len(lottery_bets)
                     
                 elif msg_type == protocol.MessageType.FINISH:
-                    # Calculate winners
-                    winners = []
-                    for bet in self.lottery.load_bets():
-                        if self.lottery.has_won(bet):
-                            winners.append(bet)
+                    # Increment finished agencies counter
+                    with self.agencies_finished_lock:
+                        self.agencies_finished += 1
+                        logger.info(
+                            "agency-finished",
+                            logger.LogResult.success,
+                            "agency-id",
+                            client_agency_id,
+                            "agencies-finished",
+                            self.agencies_finished,
+                            "quorum-min",
+                            self.quorum_min,
+                        )
+                        
+                        # Wait for quorum to be reached
+                        while self.agencies_finished < self.quorum_min:
+                            self.quorum_condition.wait()
+                        
+                        # Calculate winners only once
+                        if not self.winners_calculated:
+                            self.winners = []
+                            for bet in self.lottery.load_bets():
+                                if self.lottery.has_won(bet):
+                                    self.winners.append(bet)
+                            self.winners_calculated = True
+                            logger.info(
+                                "lottery-draw",
+                                logger.LogResult.success,
+                                "total-winners",
+                                len(self.winners),
+                            )
+                        
+                        # Notify other waiting threads
+                        self.quorum_condition.notify_all()
+                    
+                    # Filter winners for this agency only
+                    agency_winners = [
+                        winner for winner in self.winners 
+                        if winner.agency_id == client_agency_id
+                    ]
                     
                     # Convert lottery bets to protocol bets
                     protocol_winners = []
-                    for winner in winners:
+                    for winner in agency_winners:
                         protocol_winners.append(protocol.Bet(
                             agency_id=winner.agency_id,
                             first_name=winner.first_name,
@@ -69,7 +116,9 @@ class Server:
                         "bets-received",
                         bets_received,
                         "winners-sent",
-                        len(winners),
+                        len(agency_winners),
+                        "agency-id",
+                        client_agency_id,
                     )
                     return
                     
@@ -93,4 +142,6 @@ class Server:
                     raise e
                 logger.info(action, logger.LogResult.success)
 
-                self._handle_client(client_socket)
+                # Handle client in a separate thread
+                client_thread = threading.Thread(target=self._handle_client, args=(client_socket,))
+                client_thread.start()
